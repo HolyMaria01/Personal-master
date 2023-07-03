c4f4d0113797c8967a7a8bf87c481aa9f1dda03c
Maarja-Liis <maarjaliisuke@gmail.com>
2023-07-03 17:58:03 +0300

    mooduld põhi, minu parandused, kontaktid ära

--- Personal-master/assets/js/main.js
+++ Personal-master/assets/js/main.js
@@ -228,33 +228,6 @@
   });
 
   /**
-   * Lightbox picture slider
-   
-  var slideIndex = 1;
-  showSlide(slideIndex);
-  
-  function changeSlide(n) {
-    showSlide(slideIndex += n);
-  }
-  
-  function showSlide(n) {
-    var slides = document.getElementsByClassName("lightbox-slides")[0].getElementsByTagName("img");
-    
-    if (n > slides.length) {
-      slideIndex = 1;
-    } else if (n < 1) {
-      slideIndex = slides.length;
-    }
-    
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
-  }
-*/
-
-  /**
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
